291770393dc8428714f10a19e52a0c985b966797
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:07:40 -0300

    solucion reto 15

--- reto15/app.js
+++ reto15/app.js
@@ -1,1 +1,13 @@
+/* soluciones reto 15: recorrerlo y mientras lo hace que sume 
+s*/
+function computeSumOfAllElements(arr) {
+    var suma= 0
+    for(var i=0; i<arr.length; i++){
+        suma += arr[i]
+    }
+    return suma
+}
 
+//datos del ejemplo
+var output = computeSumOfAllElements([1, 2, 3]);
+console.log(output);
