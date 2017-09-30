27a8757fc75ad925170b95d7a4205f0df7eee445
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 02:52:51 -0300

    solucion reto 11

--- reto11/app.js
+++ reto11/app.js
@@ -1,1 +1,24 @@
+/*solucion reto 11: este hay varias maneras de hacerlo y es largo, 
+el arr que es un arreglo lo recorri con el for como siempre,
+para poder encontrar el texto mas corto, hay que tener cuidado cuando el arr que
+te dan son por ejemplo solo numeros, en ese caso return '' 
 
+seguir instrucciones del enunciado*/
+
+function findShortestWordAmongMixedElements(arr) {
+    var cadenaMasCorta= 'abcdefghijklmnopqrstuvwwxyz';
+    for(var i = 0; i < arr.length; i++){
+        if(cadenaMasCorta.length > arr[i].length){
+            cadenaMasCorta = arr[i]
+        }
+        
+    }if(arr.length === 0 || cadenaMasCorta==='abcdefghijklmnopqrstuvwwxyz'){
+        return ''
+    }else{
+        return cadenaMasCorta
+    }
+}
+
+//dato del ejemplo
+var output = findShortestWordAmongMixedElements([4, 'two', 2, 'three']);
+console.log(output);
