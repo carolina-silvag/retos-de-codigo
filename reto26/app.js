95a1345658f17570e1795c1ecdefc77efb7360fd
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:51:29 -0300

    solucion reto 26

--- reto26/app.js
+++ reto26/app.js
@@ -1,1 +1,20 @@
+function convertScoreToGrade(score) {
+   if(score <= 100 && score >=90) {
+       return 'A'
+   }else if(score <90 && score >=80 ){
+       return 'B'
+   }else if(score <80 && score >=70){
+       return 'C'
+   }else if(score <70 && score>=60){
+       return 'D'
+   }else if(score <60 && score >=0){
+       return 'F'
+   }else{
+       return 'PUNTUACION INVALIDA'
+   }
+}
 
+
+//datos del ejemplo
+var output = convertScoreToGrade(91);
+console.log(output);
