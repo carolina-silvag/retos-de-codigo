ff822c21d5df4cee0b2e42f54c7e2da3ce43c583
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:26:09 -0300

    solucion reto 19

--- reto19/app.js
+++ reto19/app.js
@@ -1,1 +1,11 @@
+/* solucion reto 19: muy parecido a uno anterior
+hay que saber que Object.keys(objeto) llama las propiedades 
+de un objeto y los entrega en un array y Object.values(objeto)
+llama a los valores y los entrega en un array*/
+function listAllValues(obj) {
+  return Object.values(obj)
+}
 
+//datos ejemplo
+var result = listAllValues({name: 'Krysten', age: 33, hasPets: false})
+console.log(result);
