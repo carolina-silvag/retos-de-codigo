5b2b7f75bf884118b84c49cf3e441672b9510397
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 02:08:10 -0300

    solucion reto 4

--- reto4/app.js
+++ reto4/app.js
@@ -1,1 +1,22 @@
+/*solucion reto 4: tenia que eliminar una propiedad que tuviera una cadena de texto
+y esta tuviera un largo mayor a num, utilize for para recorrer solo una parte de obj 
+debido a que un objeto no se puede recorrer con length (ver materia ahi sale la forma)*/
 
+function removeStringValuesLongerThan(num, obj) {
+    var array= Object.keys(obj)
+    for(var i = 0; i< array.length; i++){
+        if (obj[array[i]].length >= num){   
+            delete obj[array[i]]
+        }
+    }
+    return obj
+}
+
+// datos del ejemplo
+var obj = {
+  name: 'Montana',
+  age: 20,
+  location: 'Texas'
+};
+removeStringValuesLongerThan(6, obj);
+console.log(obj);
