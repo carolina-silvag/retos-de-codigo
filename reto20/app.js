405ded543327d887f441cc935281950a48c5eb4c
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:29:46 -0300

    solucion reto 20

--- reto20/app.js
+++ reto20/app.js
@@ -1,1 +1,21 @@
-
+/* solucion reto 20: complicado de explicar por aca
+tiene muchos pasos a seguir, en este caso es importante crear 
+variables vacias para array y objeto para poder guardar valores*/
+function transformEmployeeData(array) {
+  var final=[]
+  var obj1={}
+  var obj2={}
+  for(var i=0; i < array.length; i++){
+      for(var j= 0; j<array[i].length; j++){
+          if (i===0){
+              obj1[array[i][j][0]]= array[i][j][1]
+          }
+          if(i===1){
+              obj2[array[i][j][0]]= array[i][j][1]
+          }
+      }
+  }
+  final.push(obj1)
+  final.push(obj2)
+  return final
+}
