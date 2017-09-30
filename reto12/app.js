cfcaf350afe0e561081bc1b3c2caad3cdc388e79
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 02:56:38 -0300

    solucion reto 12

--- reto12/app.js
+++ reto12/app.js
@@ -1,1 +1,19 @@
+/* solucion reto 12: es parecido al anteror, a mi siempre me falta colocar punto/coma 
+pero igual funciona*/
+function findSmallestNumberAmongMixedElements(arr) {
+    var numeroMasChico = 10000000;
+    for(var i = 0; i < arr.length; i++){
+        if(numeroMasChico>arr[i]){
+            numeroMasChico = arr[i]
+        }
+        
+    }if(arr.length === 0 ||numeroMasChico === 10000000){
+        return ''
+    }else{
+        return numeroMasChico
+    }
+}
 
+//datos del ejemplo
+var output = findSmallestNumberAmongMixedElements([4, 'lincoln', 9, 'octopus']);
+console.log(output);
