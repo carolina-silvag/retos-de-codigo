062ae71d3acdbc78bf30c2e76fc6f8114b4e6826
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:53:53 -0300

    solucion reto 25

--- reto25/app.js
+++ reto25/app.js
@@ -1,1 +1,8 @@
+function calculateBillTotal(preTaxAndTipAmount) {
+    var agregar = preTaxAndTipAmount * 0.245
+    return agregar + preTaxAndTipAmount
+}
 
+//datos del ejemplo
+var output = calculateBillTotal(20);
+console.log(output);
