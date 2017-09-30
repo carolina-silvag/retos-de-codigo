be47236de634ddfbe968903b3c672c75bb058f27
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:01:39 -0300

    solucion reto 13

--- reto13/app.js
+++ reto13/app.js
@@ -1,1 +1,23 @@
+/* solucion reto 13: mas de lo mismo, hay que tener cuidado con 
+las condiciones que te pieden*/
+
+function getLongestWordOfMixedElements(arr) {
+    var cadenaMasLarga= '';
+    for(var i = 0; i < arr.length; i++){
+        if(cadenaMasLarga.length < arr[i].length){
+            cadenaMasLarga = arr[i]
+        }
+        
+    }if(arr.length === 0){
+        return ''
+    }else if(cadenaMasLarga===''){
+        return ''
+    }else{
+        return cadenaMasLarga
+    }
+}
+
+//dato del ejemplo
+var output = getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1]);
+console.log(output);
 
