972cfe7f18353d58d9fb482618790dc938cdbfbf
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:32:50 -0300

    solucion reto 21

--- reto21/app.js
+++ reto21/app.js
@@ -1,1 +1,17 @@
+/* solucion reto 21*/
+function computeSumBetween(num1, num2) {
+    var suma= 0;
+    if(num2<num1){
+        return 0
+    }else{
+        for(var i = num1; i<num2; i++){
+            suma += i
+        }
+    }
+    return suma
+}
 
+
+//datos del ejemplo
+var output = computeSumBetween (2, 5);
+console.log (salida);
