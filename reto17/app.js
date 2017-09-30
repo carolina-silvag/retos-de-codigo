c1ae255e4d5ae0fe6d391f5f0a42c990cfc076b8
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:17:06 -0300

    solucion reto 17

--- reto17/app.js
+++ reto17/app.js
@@ -1,1 +1,10 @@
+/* solucion reto 17: principalmente saber como
+se utiliza un objeto*/
 
+function getAllKeys(obj) {
+  return Object.keys(obj)
+}
+
+//dato del ejemplo
+var key=getAllKeys({name: 'Sam', age: 25, hasPets: true})
+console.log(key);
