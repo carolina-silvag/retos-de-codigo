4ac477d097acc57990bd03308e96bbdd1b453893
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:13:01 -0300

    solucion reto 16

--- reto16/app.js
+++ reto16/app.js
@@ -1,1 +1,13 @@
+/* solucion reto 16: este tiene varias maneras de hacerse
+yo utilize esta porque me acomodaba, pero tmbn se 
+podia utilizar slice para cortar y guardar en variables el valor*/
 
+function transformFirstAndLast(array) {
+    var objeto ={}
+    objeto[array.shift()] = array.pop()
+    return objeto
+}
+
+//datos del ejemplo
+var primeroUltimo=transformFirstAndLast(['Kevin', 'Bacon', 'Amor', 'Hart', 'Costner', 'Spacey'])
+console.log(primeroUltimo);
