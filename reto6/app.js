32fb27caf28d4eb6b1f6e42ae9a74e247e2b2cf8
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 02:17:31 -0300

    solucion reto 6

--- reto6/app.js
+++ reto6/app.js
@@ -1,1 +1,12 @@
 
+function areValidCredentials(name, password) {
+    if(name.length > 3 && password.length > 8){
+        return true
+    }else{
+        return false
+    }
+}
+
+//datos del ejemplo
+var output = areValidCredentials('Ritu', 'mylongpassword');
+console.log(output);
