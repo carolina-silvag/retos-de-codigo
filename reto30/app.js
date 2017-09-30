1beeb530e642edf373b44186b064255919c10c29
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:43:27 -0300

    solucion reto 30

--- reto30/app.js
+++ reto30/app.js
@@ -1,1 +1,13 @@
+function addFullNameProperty(obj) {
+    var nombreArray= Object.values(obj)
+    obj['fullName']=nombreArray.join(' ')
+    return obj
+}
 
+//datos del ejemplo
+var person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+addFullNameProperty(person);
+console.log(person.fullName);
