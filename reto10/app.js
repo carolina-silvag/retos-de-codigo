d001249ef25b2ceaaf73091cd2060e93e13f6afe
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 02:47:02 -0300

    solucion reto 10

--- reto10/app.js
+++ reto10/app.js
@@ -1,1 +1,17 @@
+/*solucion ejercicio 10: este es lo mismo de siempre un array siempre
+se recorre con for, el if tiene una condicion cuando es par el resto(%) siempre es 
+cero, por lo cual si es distinto de cero es impar por eso es (!==) este significa distinto
+de cero en nuestro caso*/
+function filterOddElements(array) {
+    var nuevoArreglo=[];
+    for(var i=0; i<array.length; i++){
+        if(array[i]% 2 !== 0){      
+            nuevoArreglo.push(array[i])
+        }
+    }
+    return nuevoArreglo
+}
 
+//datos del ejemplo
+var output = filterOddElements([1, 2, 3, 4, 5]);
+console.log(output);
