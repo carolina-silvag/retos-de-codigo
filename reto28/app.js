b5c763cd01e893878577abfbe4dec8cd118f0e13
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:47:09 -0300

    solucion reto 28

--- reto28/app.js
+++ reto28/app.js
@@ -1,1 +1,22 @@
+function getProductOfAllElementsAtProperty(obj, key) {
+    var producto = 1
+    if(typeof obj[key]!="object"){
+        return 0
+    }else if(Object.keys(obj).indexOf(key) == -1){
+        return 0
+    }else if(obj[key].length === 0){
+        return 0
+    }else{
+        for(var i=0; i<obj[key].length; i++){
+            producto *= obj[key][i]
+        }
+    }
+    return producto
+}
 
+//datos ejemplo
+var obj = {
+key: [1, 2, 3, 4];
+};
+var output = getProductOfAllElementsAtProperty(obj, 'key');
+console.log(output);
