3cf4271cdf9d6cb8378416cb81b34f9f91eb709c
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:04:17 -0300

    solucion reto 14

--- reto14/app.js
+++ reto14/app.js
@@ -1,1 +1,19 @@
+function getLargestNumberAmongMixedElements(arr) {
+    var numeroMayor= 0
+    for(var i = 0; i<arr.length; i++){
+        if(numeroMayor< arr[i]){
+            numeroMayor=arr[i]
+        }
+    }
+    if(arr.length === 0){
+        return 0
+    }else if(numeroMayor === 0 || numeroMayor === true){
+        return 0
+    }else{
+        return numeroMayor
+    }
+}
 
+//datos del ejemplo
+var output = getLargestNumberAmongMixedElements([3, 'word', 5, 'up', 3, 1]);
+console.log(output);
