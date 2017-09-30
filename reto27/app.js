4d36b9f54596852cfe38bad2eb1432b830fbf188
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:49:14 -0300

    solucion reto 27

--- reto27/app.js
+++ reto27/app.js
@@ -1,1 +1,22 @@
+function getSumOfAllElementsAtProperty(obj, key) {
+    var suma = 0
+    if(typeof obj[key]!="object"){
+        return 0
+    }else if(Object.keys(obj).indexOf(key) == -1){
+        return 0
+    }else if(obj[key].length === 0){
+        return 0
+    }else{
+        for(var i=0; i<obj[key].length; i++){
+            suma += obj[key][i]
+        }
+    }
+    return suma
+}
 
+//datos ejemplo
+var obj = {
+key: [4, 1, 8];
+};
+var output = getSumOfAllElementsAtProperty(obj, 'key');
+console.log(output);
