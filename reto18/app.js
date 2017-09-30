a919ea7e56e7d61b1e77e532bfe15f23b3f8599d
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:20:48 -0300

    solucion reto 18

--- reto18/app.js
+++ reto18/app.js
@@ -1,1 +1,13 @@
+/* solucion reto 18: saber como crear objetos 
+y como utilizar array (revizar materia)*/
 
+function fromListToObject(array) {
+  var lista = {}
+  for (var i= 0; i< array.length; i++){
+      lista[array[i][0]] = array[i][1]
+  }
+  return lista
+}
+
+var resultado=fromListToObject([['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]])
+console.log(resultado);
