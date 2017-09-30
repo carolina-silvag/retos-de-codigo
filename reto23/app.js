d7d1b214f3ac7c703f281391aca403fd681982e0
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 03:59:33 -0300

    solucion 23 actualizada a la correcta

--- reto23/app.js
+++ reto23/app.js
@@ -1,13 +1,12 @@
-function findShortestOfThreeWords(word1, word2, word3) {
-    if(word1.length <=word2.length && word1.length <= word3.length ){
+function getLongestOfThreeWords(word1, word2, word3) { 
+    if(word1.length >=word2.length && word1.length >= word3.length ){
         return word1
-    }else if(word2.length < word1.length && word2.length <= word3.length){
+    }else if(word2.length > word1.length && word2.length >= word3.length){
         return word2
-    }else if(word3.length < word1.length && word3.length < word2.length){
+    }else if(word3.length > word1.length && word3.length > word2.length){
         return word3
     }
 }
 
-//datos del ejemplo
-var output = findShortestOfThreeWords('a', 'dos', 'tres');
+var salida = getLongestOfThreeWords('estos', 'tres', 'palabras');
 console.log(salida);
