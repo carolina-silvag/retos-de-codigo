762c48ed6f2dcdd05fa1172019336625d5bdec58
carolina-silvag <carolinasguzman@gmail.com>
2017-09-30 01:51:45 -0300

    solucion reto 2

--- reto2/app.js
+++ reto2/app.js
@@ -1,1 +1,11 @@
+//solucion reto 2: buscar como se agrega a un object (ver materia)
+function addArrayProperty(word1, word2, word3) {
+    return word1[word2]=word3;       
+}
 
+
+//ejemplo de prueba
+var myObj = {};
+var myArray = [1, 3];
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(myObj.myProperty);
