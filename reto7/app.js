e79e005822359e88b58ac108c28a6dc2cf44110d
AlejandraIsabel <jannyway8727@gmail.com>
2017-10-01 13:37:07 -0300

    otra forma

--- reto7/app.js
+++ reto7/app.js
@@ -20,3 +20,26 @@ var obj = {
 };
 var output = getElementOfArrayProperty(obj, 'key', 0);
 console.log(output); 
+
+// Otra forma de hacerlo, se crea una variable que contiene el arreglo  (los valores del key).
+// se crea la condicion de que si la propiedad no es un array, devuelve undefined 
+// se crea otra condicion de que si el indice dentro de key es mayor a los que contiene, devuelve undefined. Ademas esta condicion comprueba que si el array esta vacio
+// por ultimo se crea una condicion de que si el array no existe, devuelve undefined ( cuando llamo a algo que no existe, la consola devuelve "undefined")
+// luego se filtrar la informacion, se comienza a trabjar buscando el index dentro del array y esto devuelve el elemento del index
+  
+/*function getElementOfArrayProperty(obj, key, index) {
+   var x = obj.key;
+      
+   if (typeof x != "object" ){
+        return undefined;
+   }
+   if (key[index] === undefined){
+        return undefined
+   }
+   if (obj.key === undefined){
+        return undefined
+   }
+   var y = x[index];
+   return y
+    
+ }*/
